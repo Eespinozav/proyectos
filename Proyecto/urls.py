3992d00329ba912adbf21cc9e6ade9849a0103c1
Cristoph <cristopher.alejandro@gmail.com>
2017-06-09 20:51:42 -0400

    heroku files

--- Proyecto/urls.py
+++ Proyecto/urls.py
@@ -15,6 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
